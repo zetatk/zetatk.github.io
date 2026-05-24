3412a50a47c3896e4b723f6200584236ec295031
zeta-tk <admin@zetatk.org>
2026-05-24 23:51:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Document ready:
 $(document).ready(function() {
     /**version: */
-    const version = '1.0.0.1';
+    const version = '1.0.0.4';
+    document.getElementById("app-version").textContent = currentVersion;
     console.log(version);
   });
